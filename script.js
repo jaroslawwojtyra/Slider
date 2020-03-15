034f4596d5dee1f8eb5eb4a5f4922bba9d5627df
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-03-15 11:36:51 +0100

    Slider v. 0.1

---
+++ script.js
@@ -0,0 +1,5 @@
+// suwak kilometry w mieście
+// suwak poza miastem
+// obiekt fuel - cena + spalanie benzyna (miasto + poza)
+// + spalanie hybryda (miasto + poza)
+// obiekt cena - zależny od kilometrów do przejechania
